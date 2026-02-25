49d0ab04ad4bd9cb2917622fe21a0bb5022a9c3e
lukasz <lukasz.darmofal@cyfroweszkoly.pl>
2026-02-25 09:34:33 +0100

    Create seeding scripts

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,1 +1,20 @@
 
+db = db.getSiblingDB("mongoflix")
+db.movies.drop()
+db.reviews.drop()
+
+console.log("Seedowanie rozpoczęte!")
+
+const moviesData = require("fs").readFileSync("data/movies.json", "utf8")
+const moviesList = JSON.parse(moviesData)
+db.movies.insertMany(moviesList)
+
+console.log("dodano filmy")
+
+const reviewsData = require("fs").readFileSync("data/reviews.json", "utf8")
+const reviewsList = JSON.parse(reviewsData)
+db.reviews.insertMany(reviewsList)
+
+console.log("dodano opinie i zakończono")
+
+
