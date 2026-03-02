c08cca0ec55cb30a94e33ce4d65b89174f0387c6
Marsel <castalanoyt@gmail.com>
2026-03-02 20:09:59 +0100

    first question

--- queries/analysis.mongodb.js
+++ queries/analysis.mongodb.js
@@ -1,2 +1,19 @@
 // Movies in the action category after 2020
 
+print("\n - Movies in the 'Action' category after 2020 -");
+
+db.movies.aggregate([
+  {
+    $match: {
+      movieGenres: "action",
+      movieRelease: { $gt: 2020 }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      movieName: 1,
+      movieRelease: 1
+    }
+  }
+]).forEach(doc => printjson(doc));
